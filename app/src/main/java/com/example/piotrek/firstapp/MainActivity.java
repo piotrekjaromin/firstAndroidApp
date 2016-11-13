bb8f2e03fb9c841b5d1a9895708b7239be2a6a62
PiotrJaromin <piotrek.jaromin@gmail.com>
2016-11-13 20:39:41 +0100

    General structure of app

---
+++ app/src/main/java/com/example/piotrek/firstapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.piotrek.firstapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        //Uri.parse("http://www.example.com"));
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_MAIN);
+                i.addCategory(Intent.CATEGORY_APP_CALCULATOR);
+                startActivity(i);
+            }
+        });
+
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("tel:9510300000"));
+                startActivity(i);
+            }
+        });
+    }
+}
